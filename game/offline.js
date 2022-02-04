a6890d734511a2fa6ad15cfe5b4ad32cd848718b
Bruno Vitetti <bvitetti@gmail.com>
2022-02-04 15:04:55 -0300

    First Commit
    First commit

---
+++ game/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1581032207,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"g1images/rogue-sheet0.png",
+		"g1images/rogue-sheet1.png",
+		"g1images/rogue-sheet2.png",
+		"g1images/grass.png",
+		"g1images/invisiblecolission-sheet0.png",
+		"g1images/grasscorner_r.png",
+		"g1images/grasscorner_l.png",
+		"g1images/dirtblock.png",
+		"g1images/dirtblockcorner_l.png",
+		"g1images/dirtblockcorner_r.png",
+		"g1images/dirtbottom.png",
+		"g1images/slime-sheet0.png",
+		"g1images/slime-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
